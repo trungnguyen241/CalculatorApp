26bdb6b362cb44e3396d6a82aa899a27b58ff397
Trung <trung.a.nguyen@wsu.edu>
2015-04-19 13:55:17 -0700

    Added test cases for CalculatorBrain.

--- CalculatorApp/CalculatorTests/CalculatorTests.m
+++ CalculatorApp/CalculatorTests/CalculatorTests.m
@@ -14,12 +14,10 @@
 @interface CalculatorTests : XCTestCase{
     @private
         ViewController *calcViewController;
-        CalculatorBrain *calcBrain;
+        CalculatorBrain *brain;
         UIView *view;
 }
 
-//@property ViewController *calcViewController;
-//@property CalculatorBrain *calcBrain;
 @end
 
 @implementation CalculatorTests
@@ -27,9 +25,9 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
     calcViewController = [[ViewController alloc] init];
-    calcBrain = [[CalculatorBrain alloc] init];
+    brain = [[CalculatorBrain alloc] init];
     view = calcViewController.view;
 }
 
@@ -39,4 +37,116 @@
     [super tearDown];
 }
 
+-(NSString *)formatInputString:(NSArray *)inputString
+{
+    NSString *formatString = [[NSString alloc] init];
+    NSNumberFormatter* nf = [[NSNumberFormatter alloc] init];
+    
+    for (NSString *str in inputString)
+    {
+        if ([nf numberFromString: str] != nil)
+        {
+            [brain setOperand: [str doubleValue]];
+        }
+        
+        if ([[brain singleOperationList] containsObject: str])
+        {
+            [brain performSingleOperation:str];
+        }
+        
+        formatString = [brain formatInputString:str];
+    }
+    
+    return formatString;
+}
+
+-(double)calculateFromString
+{
+    return [brain calculateFromEquationString];
+}
+
+-(void)testValidInputString
+{
+    NSArray *inputString = [[NSArray alloc] initWithObjects:@"2", @"+", @"3", @"x", @"4", @"-", @"5", nil];
+ 
+    NSString *formatString = [self formatInputString:inputString];
+    
+    XCTAssertEqualObjects(formatString, @"2+3x4-5");
+}
+
+-(void)testAnOperatorFollowBySingleOperator
+{
+    NSArray *inputString = [[NSArray alloc] initWithObjects:@"2", @"+", @"±", @"3", @"x", @"4", nil];
+   
+    NSString *formatString = [self formatInputString:inputString];
+    
+    XCTAssertEqualObjects(formatString, @"2+3x4");
+}
+
+-(void)testACButtonPressed
+{
+    NSArray *inputString = [[NSArray alloc] initWithObjects:@"2", @"+", @"3", @"AC", @"4", @"+", @"5", nil];
+   
+    NSString *formatString = [self formatInputString:inputString];
+    
+    XCTAssertEqualObjects(formatString, @"4+5");
+}
+
+-(void)testDelButtonPressed
+{
+    NSArray *inputString = [[NSArray alloc] initWithObjects:@"2", @"+", @"3", @"x", @"4", @"←", @"←", nil];
+  
+    NSString *formatString = [self formatInputString:inputString];
+    
+    XCTAssertEqualObjects(formatString, @"2+3");
+}
+
+-(void)testContinuousOperatorPressed
+{
+    NSArray *inputString = [[NSArray alloc] initWithObjects:@"2", @"+", @"x", @"/", @"3", @"x", @"4", nil];
+    
+    NSString *formatString = [self formatInputString:inputString];
+    
+    XCTAssertEqualObjects(formatString, @"2/3x4");
+}
+
+-(void)testContinuousDigitPressed
+{
+    NSArray *inputString = [[NSArray alloc] initWithObjects:@"2", @"23", @"234", @"x", @"5", @"56", @"567", nil];
+    
+    NSString *formatString = [self formatInputString:inputString];
+    
+    XCTAssertEqualObjects(formatString, @"234x567");
+}
+
+-(void)testSingleOperationPressed
+{
+    NSArray *inputString = [[NSArray alloc] initWithObjects:@"2", @"%", @"+", @"3", @"x", @"4", @"±", @"%", nil];
+   
+    NSString *formatString = [self formatInputString:inputString];
+    
+    XCTAssertEqualObjects(formatString, @"0.02+3x-0.04");
+}
+
+-(void)testAnOperatorFirstPressed
+{
+    NSArray *inputString = [[NSArray alloc] initWithObjects:@"+", @"2", nil];
+    
+    NSString *formatString = [self formatInputString:inputString];
+    
+    XCTAssertEqualObjects(formatString, @"0+2");
+}
+
+-(void)testAddition
+{
+    double result;
+    NSArray *inputString = [[NSArray alloc] initWithObjects:@"2", @"+", @"3", @"+", @"34", nil];
+    
+    NSString *formatString = [self formatInputString:inputString];
+    result = [self calculateFromString];
+    
+    XCTAssertEqualObjects(formatString, @"2+3+34");
+    XCTAssertEqual(result, 39);
+
+}
 @end
